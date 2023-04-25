3294cea47867d17b6f56e233532bd206049b2122
Chris020103 <Chrism@caiway.net>
2023-04-25 14:06:02 +0200

    Build

--- src/js/game.js
+++ src/js/game.js
@@ -5,12 +5,11 @@ import { Resources, ResourceLoader } from './resources.js'
 export class Game extends Engine {
 
     constructor() {
-        super({ width: 800, height: 600 })
+            super({ width: 800, height: 600 })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
     startGame() {
-        console.log("start de game!")
         const fish = new Actor()
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(400, 300)
